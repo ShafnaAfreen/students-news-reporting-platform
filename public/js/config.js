be181f73a8cc1a49a2419e073c902ebe6a0ed1a9
ShafnaAfreen <shafnaafreen18@gmail.com>
2025-04-29 15:27:31 +0530

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 // Configure your cloud name, API key and API secret:
 
 const myconfig = cloudinary.config({
-  cloud_name: 'dpz1hvykp',
-  api_key: '434752942583556',
-  api_secret: 'UJb033esUOJ7gFe5itCviX4OtPg',
+  cloud_name: 'cloudname',
+  api_key: 'apikey',
+  api_secret: 'apisecret',
   secure: true
 });
 
